0007491e9a47ec08ab232353940e2e7ce3689bc2
Kai-Jen Tsai <kai@cognitosolution.com>
2025-08-16 00:00:18 +0000

    ok why is it not workng

--- src/components/auth-dialog.tsx
+++ src/components/auth-dialog.tsx
@@ -2,16 +2,15 @@
 'use client'
 
 import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
 import { Sprout } from "lucide-react"
 import Link from "next/link"
@@ -22,27 +21,29 @@ interface AuthDialogProps {
 
 export function AuthDialog({ children }: AuthDialogProps) {
   return (
-    <AlertDialog>
-      <AlertDialogTrigger asChild>
+    <Dialog>
+      <DialogTrigger asChild>
         {children}
-      </AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle className="flex items-center gap-2">
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="flex items-center gap-2">
             <Sprout className="w-6 h-6 text-primary"/>
             Create an Account to Continue
-          </AlertDialogTitle>
-          <AlertDialogDescription>
+          </DialogTitle>
+          <DialogDescription>
             To access features like booking, messaging, and saving favourites, you'll need to be logged in. Join our community to connect with top beauty professionals.
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Continue as Guest</AlertDialogCancel>
-          <AlertDialogAction asChild>
-            <Link href="/signup">Log In / Sign Up</Link>
-          </AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+            <DialogClose asChild>
+                <Button variant="ghost">Continue as Guest</Button>
+            </DialogClose>
+            <Button asChild>
+                <Link href="/signup">Log In / Sign Up</Link>
+            </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   )
 }
