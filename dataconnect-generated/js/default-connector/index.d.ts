32c41416f04f2c58d6fda4d0650f14382398effd
Kai-Jen Tsai <kai@cognitosolution.com>
2025-08-18 11:05:52 +0000

    ok

--- dataconnect-generated/js/default-connector/index.d.ts
+++ dataconnect-generated/js/default-connector/index.d.ts
@@ -8,7 +8,36 @@ export type Int64String = string;
 export type DateString = string;
 
 
+export interface listServicesResponse {
+  services: Service[];
+  __typename?: 'listServicesResponse';
+}
 
+export interface listServicesArgs {
+
+}
+
+export declare function listServices(
+  dataConnect: any,
+  args: listServicesArgs
+): Promise<listServicesResponse>;
+
+export interface insertUserResponse {
+  data: User;
+  __typename?: 'insertUserResponse';
+}
+
+export interface insertUserArgs {
+  id: UUIDString;
+  name: string;
+  email: string;
+  role: string;
+}
+
+export declare function insertUser(
+  dataConnect: any,
+  args: insertUserArgs
+): Promise<insertUserResponse>;
 
 export interface Booking_Key {
   id: UUIDString;
@@ -40,3 +69,74 @@ export interface User_Key {
   __typename?: 'User_Key';
 }
 
+export interface Booking {
+  id: UUIDString;
+  providerId: UUIDString;
+  providerName: string;
+  serviceIds: string[];
+  date: TimestampString;
+  status: string;
+  clientName?: string;
+  isPaid: boolean;
+  reviewId?: string;
+  __typename?: 'Booking';
+}
+
+export interface Review {
+  id: UUIDString;
+  bookingId: UUIDString;
+  author: string;
+  avatarUrl?: string;
+  dataAiHint?: string;
+  rating: number;
+  comment: string;
+  title?: string;
+  __typename?: 'Review';
+}
+
+export interface ServiceCategory {
+  id: UUIDString;
+  name: string;
+  services: Service[];
+  __typename?: 'ServiceCategory';
+}
+
+export interface ServiceProvider {
+  id: UUIDString;
+  userId: UUIDString;
+  name: string;
+  specialty: string;
+  avatarUrl: string;
+  dataAiHint?: string;
+  rating: number;
+  reviewCount: number;
+  isFeatured?: boolean;
+  isFavourite?: boolean;
+  bio: string;
+  location: string;
+  playlist: string;
+  user: User;
+  services: Service[];
+  reviews: Review[];
+  __typename?: 'ServiceProvider';
+}
+
+export interface Service {
+  id: UUIDString;
+  categoryId: UUIDString;
+  name: string;
+  description: string;
+  price: number;
+  duration: number;
+  provider: ServiceProvider[];
+  __typename?: 'Service';
+}
+
+export interface User {
+  id: UUIDString;
+  name: string;
+  email: string;
+  role: string;
+  provider: ServiceProvider[];
+  __typename?: 'User';
+}
