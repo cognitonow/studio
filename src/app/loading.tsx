b2f68e5cb635fe1a9979ed236a4c579f7ed26764
Kai-Jen Tsai <kai@cognitosolution.com>
2025-08-21 22:26:38 +0000

    can the loading screen be interaactive

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,9 +2,7 @@ export default function Loading() {
   // This loading UI will be displayed as a fallback while the page content is loading.
   return (
     <div className="flex h-[calc(100vh-10rem)] items-center justify-center">
-      <div className="text-2xl font-semibold text-muted-foreground">
-        Loading...
-      </div>
+      <div className="h-16 w-16 animate-spin rounded-full border-4 border-primary border-t-transparent"></div>
     </div>
   );
 }
